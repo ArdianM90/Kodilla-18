550c8536726ecafd8b351d94724e2eb83ebf543f
unknown <amienkowski@gmail.com>
2021-01-06 08:58:56 +0100

    Dodane filtrowanie tablic.

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -21,6 +21,9 @@ public class TrelloController {
 
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+        trelloBoards.stream()
+                .filter(e -> (e.getId() != null && e.getName() != null && e.getName().contains("Kodilla")))
+                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+
     }
 }
